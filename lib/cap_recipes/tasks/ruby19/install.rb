941f0a3b651a015da2b8954f5a86e8bddfa58295
Donovan Bray <donovan@homerun.com>
2011-07-22 18:08:43 -0700

    keep the bz2 file so wget doesn't have to download the same file again unless it changes

--- lib/cap_recipes/tasks/ruby19/install.rb
+++ lib/cap_recipes/tasks/ruby19/install.rb
@@ -13,7 +13,7 @@ Capistrano::Configuration.instance(true).load do
     task :install, :roles => :app do
       utilities.apt_install %w[build-essential zlib1g-dev libssl-dev openssl libcurl4-openssl-dev libreadline6-dev bzip2]
       sudo "mkdir -p /usr/local/src"
-      run "cd /usr/local/src && #{sudo} wget --tries=2 -c --progress=bar:force #{ruby_src} && #{sudo} rm -f #{ruby_ver}.tar && #{sudo} bunzip2 #{ruby_ver}.tar.bz2 && #{sudo} tar xvf #{ruby_ver}.tar"
+      run "cd /usr/local/src && #{sudo} wget --tries=2 -c --progress=bar:force #{ruby_src} && #{sudo} bunzip2 --keep --force #{ruby_ver}.tar.bz2 && #{sudo} tar xvf #{ruby_ver}.tar"
       run "cd /usr/local/src/#{ruby_ver} && #{sudo} ./configure --prefix=#{base_ruby_path} --enable-shared && #{sudo} make install"
     end
 
