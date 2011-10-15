62fa3cdca2466b6d92b1614502429f541dd7b270
Donovan Bray <donovan@homerun.com>
2011-10-14 17:30:23 -0700

    stopping sd-agent for specific environments should be left up to the indivudual use case

--- lib/cap_recipes/tasks/sdagent/install.rb
+++ lib/cap_recipes/tasks/sdagent/install.rb
@@ -23,7 +23,7 @@ Capistrano::Configuration.instance(true).load do
       utilities.apt_update
       utilities.apt_install "sd-agent python-mysqldb python-dev"
       sdagent.setup
-      sdagent.restart
+      sdagent.restart unless sdagent_watcher == :god
     end
 
     desc "select watcher"
@@ -47,13 +47,6 @@ Capistrano::Configuration.instance(true).load do
       sudo "mkdir -p /var/run/sd-agent"
       sudo "chown sd-agent:sd-agent /var/run/sd-agent"
       god.upload sdagent_god_path, 'sdagent.god'
-      #disable init from automatically starting and stopping sd-agent, giving ec2 instances a chance 
-      #to change thier hostname before reporting in to serverdensity
-      #but leave the init script in place to be called manally
-      sudo "update-rc.d -f sd-agent remove"
-      #if you simply remove lsb driven links an apt-get can later reinstall them
-      #so we explicitly define the kill scripts.
-      sudo "update-rc.d sd-agent stop 20 2 3 4 5 ."
     end
     
     task :update, :roles => :sdagent do
