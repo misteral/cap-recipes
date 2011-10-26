b969e38b647b2d2ee3b25d5d8c1ca3a0d141b8e8
Donovan Bray <donovan@homerun.com>
2011-10-26 15:55:47 -0700

    more docs for nginx_passenger better defaults

--- lib/cap_recipes/tasks/nginx_passenger/hooks.rb
+++ lib/cap_recipes/tasks/nginx_passenger/hooks.rb
@@ -3,7 +3,8 @@
 Capistrano::Configuration.instance(true).load do
   after "deploy:provision", "nginx_passenger:install"
   after "deploy:setup", "nginx_passenger:configure"
-  after "deploy:restart", "nginx_passenger:passenger:restart"
+  # you don't need to restart if you are using the regular capistrano timestamped deployes; I think.
+  #after "deploy:restart", "nginx_passenger:passenger:restart"
   after "logrotate:rotate", "nginx_passenger:reopen"
   on :load, "nginx_passenger:watcher"
 end
