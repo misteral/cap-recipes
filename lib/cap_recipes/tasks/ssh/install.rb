f4224e08fd24fd1748670737f7c3be28e49f90b5
Donovan Bray <donovan@homerun.com>
2012-01-10 18:57:41 -0800

    fix ssh_issue namespace and task naming

--- lib/cap_recipes/tasks/ssh/install.rb
+++ lib/cap_recipes/tasks/ssh/install.rb
@@ -1,10 +1,10 @@
 Capistrano::Configuration.instance(true).load do
 
-  namespace :ssh_issue do
+  namespace :ssh do
     set :issue_net, File.join(File.dirname(__FILE__),'issue.net')
 
     desc "Setup and Configure issue.net and sshd_config"
-    task :install do
+    task :install_issue do
       utilities.sudo_upload_template issue_net, "/etc/issue.net", :mode => "644", :owner => 'root:root'
        sudo %Q{sed -i "s/#*Banner.*/Banner \\\/etc\\\/issue.net/g" /etc/ssh/sshd_config }
        sudo %Q{sed -i "s/PrintLastLog yes/PrintLastLog no/g" /etc/ssh/sshd_config }
