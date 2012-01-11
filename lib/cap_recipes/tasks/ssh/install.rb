ac519775b3bc4c9d844a737abfa9cd947e5038e3
Rick Russell <rick.russell@homerun.com>
2012-01-10 17:58:02 -0800

    fix up ssh/install.rb to set banner file and disable PrintLastLog

--- lib/cap_recipes/tasks/ssh/install.rb
+++ lib/cap_recipes/tasks/ssh/install.rb
@@ -1,15 +1,13 @@
 Capistrano::Configuration.instance(true).load do
 
-  namespace :ssh do
+  namespace :ssh_issue do
     set :issue_net, File.join(File.dirname(__FILE__),'issue.net')
-    set :sshd_config, File.join(File.dirname(__FILE__),'sshd_config')
 
     desc "Setup and Configure issue.net and sshd_config"
-    task :install_issue do
-      utilities.sudo_upload_template, issue_net, "/etc/issue.net", :mode => "644", :owner => 'root:root'
-      utilities.sudo_upload_template, sshd_config, "/etc/ssh/sshd_config", :mode => "644", :owner => 'root:root'
-      sudo "sed -i s/#Banner \/etc\/issue.net/Banner \/etc\/issue.net/g /etc/ssh/sshd_config"
-      sudo "sed -i s/PrintLastLog yes/PrintLastLog no/g /etc/ssh/sshd_config"
+    task :install do
+      utilities.sudo_upload_template issue_net, "/etc/issue.net", :mode => "644", :owner => 'root:root'
+       sudo %Q{sed -i "s/#*Banner.*/Banner \\\/etc\\\/issue.net/g" /etc/ssh/sshd_config }
+       sudo %Q{sed -i "s/PrintLastLog yes/PrintLastLog no/g" /etc/ssh/sshd_config }
     end
 
     desc "Restart SSH"
