2a2d23da68c957dd835895e75e2d59a97d838230
Donovan Bray <donovan@homerun.com>
2012-01-10 21:23:01 -0800

    add a restart to ssh:install_issues to make sure the banner goes live.

--- lib/cap_recipes/tasks/ssh/install.rb
+++ lib/cap_recipes/tasks/ssh/install.rb
@@ -6,15 +6,14 @@ Capistrano::Configuration.instance(true).load do
     desc "Setup and Configure issue.net and sshd_config"
     task :install_issue do
       utilities.sudo_upload_template issue_net, "/etc/issue.net", :mode => "644", :owner => 'root:root'
-       sudo %Q{sed -i "s/#*Banner.*/Banner \\\/etc\\\/issue.net/g" /etc/ssh/sshd_config }
-       sudo %Q{sed -i "s/PrintLastLog yes/PrintLastLog no/g" /etc/ssh/sshd_config }
+      sudo %Q{sed -i "s/#*Banner.*/Banner \\\/etc\\\/issue.net/g" /etc/ssh/sshd_config }
+      sudo %Q{sed -i "s/PrintLastLog yes/PrintLastLog no/g" /etc/ssh/sshd_config }
+      restart
     end
 
     desc "Restart SSH"
     task :restart do
-      utilities.run_compressed %Q{
-        #{sudo} /etc/init.d/ssh restart
-      }
+      sudo "/etc/init.d/ssh restart"
     end
   end
 end
